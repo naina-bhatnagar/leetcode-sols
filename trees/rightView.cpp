fb4a2c294ec4b8e7a242cf904db6d2f17b1b6bae
naina-bhatnagar <nainaabhatnagar@gmail.com>
2026-03-21 22:19:28 +0530

    the elegant way

--- trees/rightView.cpp
+++ trees/rightView.cpp
@@ -14,21 +14,15 @@ struct TreeNode{
     }
 };
 
+void right(vector<int>&ans,int level,TreeNode *root){
+    if(root == nullptr) return;
+    if(level == ans.size()) ans.push_back(root->val);
+    right(ans,level+1,root->right);
+    right(ans,level+1,root->left);
+}
 vector<int> rightSideView(TreeNode* root) {
     vector<int> ans;
-    if(root==nullptr) return ans;
-    queue<TreeNode*>q;
-    q.push(root);
-    while(!q.empty()){
-        int n = q.size();
-        for(int i=1;i<=n;i++){
-            root = q.front();
-            if(i==n) ans.push_back(root->val);
-            if(root->left != nullptr) q.push(root->left);
-            if(root->right != nullptr) q.push(root->right);
-            q.pop();
-        }
-    }
+    right(ans,0,root);
     return ans;
 }
 
